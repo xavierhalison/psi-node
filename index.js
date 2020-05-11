777726d131429764a188c7981e2bd121776f98c8
xavierhalison <halisonpx@gmail.com>
2020-05-11 10:34:45 -0300

    adding cors rule

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = require("./router")
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+app.use(cors())
 
 router(app);
 

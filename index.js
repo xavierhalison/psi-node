3a408ec89c393d1bdd12b93677ec9b300a4b1fb6
xavierhalison <halisonpx@gmail.com>
2020-05-11 10:13:34 -0300

    setting port to 8000 due to react conflicts

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = require("./router")
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 router(app);
 
